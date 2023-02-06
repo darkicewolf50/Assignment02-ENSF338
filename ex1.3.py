4a96c249ceab8a9d781f7809284a769bb507a782
darkicewolf50 <darkicewolf50@gmail.com>
2023-02-06 15:43:22 -0700

    Exercise01 complete other than final wording and pdfing

--- ex1.3.py
+++ ex1.3.py
@@ -1,11 +1,12 @@
-
-
-def func(n):
+def func(n, memo={}):
     if n == 0 or n == 1:
         return n
+    if n in memo:
+        return memo[n]
     else:
-        return func(n-1) + func(n-2)
-
+        memo[n] = func(n-1, memo) + func(n-2, memo)
+        return memo[n]
 
 if __name__ == "__main__":
     print(func(5))
+
